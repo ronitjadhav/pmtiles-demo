1f9b5d46d62a6a41139b09ec49878b1d8f3c41af
ronitjadhav <ronitjadhav17@gmail.com>
2025-05-29 18:02:00 +0200

    testing

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',         
+  base: '/pmtiles-demo/', // Correct base for GitHub Pages
   build: {
     outDir: 'docs',
-    emptyOutDir: false
+    emptyOutDir: true // Ensure old files are cleared during build
   }
-})
+});
